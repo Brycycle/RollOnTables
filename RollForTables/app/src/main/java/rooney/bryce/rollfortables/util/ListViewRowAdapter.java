71c520157e5c8e24020af2bd68ff829fef4fca73
Brycycle <33557087+Brycycle@users.noreply.github.com>
2017-11-12 14:17:06 -0600

    Small things
    Finished fragment_create_table_view
    added view and class recource for listAdapter

---
+++ RollForTables/app/src/main/java/rooney/bryce/rollfortables/util/ListViewRowAdapter.java
@@ -0,0 +1,12 @@
+package rooney.bryce.rollfortables.util;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Brycycle on 11/12/2017.
+ */
+
+public class ListViewRowAdapter extends ArrayAdapter {
+
+
+}
